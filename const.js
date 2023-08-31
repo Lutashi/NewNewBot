947ce56ee76da973fcfeb1945360cb6490c40659
Aikhan <ajumashukurov@gmail.com>
2023-08-31 13:44:27 +0600

    i cant do object

---
+++ const.js
@@ -0,0 +1,13 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`
+const text = `
+Whats your name?
+`
+const text2 = `
+Текст ептаа
+`
+module.exports.commands = commands
+module.exports.text = text
+module.exports.text2 = text2
