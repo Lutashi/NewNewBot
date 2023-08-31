fbe56fc3b1bc7b41da04fcfb521d0bbab2a4bb1c
Aikhan <ajumashukurov@gmail.com>
2023-08-31 15:01:57 +0600

    Added a Whats your name?

--- const.js
+++ const.js
@@ -2,9 +2,5 @@ const commands = `
 /start - Перезапустить бота
 /help - Помощь
 `
-const text = `
-Whats your name?
-`
 module.exports.commands = commands
-module.exports.text = text
-module.exports.text2 = text2
+
